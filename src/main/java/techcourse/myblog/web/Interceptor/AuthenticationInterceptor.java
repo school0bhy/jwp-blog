dc05ce5fc914f40af63424d27f53ee84945286f2
school0bhy <house1217@ajou.ac.kr>
2019-07-21 02:41:19 +0900

    fix: request에 세션 설정 안된 경우 처리

--- src/main/java/techcourse/myblog/web/Interceptor/AuthenticationInterceptor.java
+++ src/main/java/techcourse/myblog/web/Interceptor/AuthenticationInterceptor.java
@@ -6,17 +6,24 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Component
 public class AuthenticationInterceptor extends HandlerInterceptorAdapter {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        Object user = request.getSession().getAttribute("user");
+        Optional<HttpSession> sessionOpt = Optional.ofNullable(request.getSession());
+        sessionOpt.ifPresent(session -> {
+                    Object user = session.getAttribute("user");
 
-        if (request.getRequestURI().equals("/logout") || user == null) {
-            return;
-        }
+                    if (request.getRequestURI().equals("/logout") || user == null) {
+                        return;
+                    }
 
-        modelAndView.addObject("user", user);
+                    modelAndView.addObject("user", user);
+                }
+        );
+
     }
 }
